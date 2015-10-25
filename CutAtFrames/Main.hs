7087f37c7b4a54c119fac820ecccc953d2036faa
Greg Hale <imalsogreg@gmail.com>
2015-10-25 16:28:55 -0400

    lots of filename and ffmpeg argument debugging

--- CutAtFrames/Main.hs
+++ CutAtFrames/Main.hs
@@ -35,31 +35,37 @@ optsParser = (,,,,) <$> optPath   "input"     'i' "Input video"
                     <*> optDouble "framerate" 'f' "Video frame rate"
                     <*> optDouble "bitesize"  'b' "Size of intermediate chunks"
 
-------------------------------------------------------------------------------
-getTimes :: FilePath -> Shell Int
-getTimes fp = fmap (read . T.unpack) $ input fp
--- TODO, how to do that safely?
+nameLength :: Int
+nameLength = 6
 
--- TODO
--- getFrameRate :: FilePath -> Shell Double
--- getFrameRate fp = do
---   res <- inshell ("ffmpeg -i " <> repr fp) empty
---   return 10
 
-type F = ((Int,Int), [Int], [(Int,(Int,Int))])
+-- type F = ((Int,Int), [Int], [(Int,(Int,Int))])
+-- clipBites :: Double -> Double -> [Int] -> Map.Map Int (Int,Int)
+-- clipBites fps biteSize clipFrames =
+--   let framesPerBite = ceiling $ biteSize * fps
+--       state0 = ((0,0), [], [])
+--       aux :: F ->  Int -> F
+--       aux ((biteFrame0,biteFrameN), accClips, accAssocList) c
+--         | c - biteFrame0 < framesPerBite =
+--           ((biteFrame0, c), c:accClips, accAssocList)
+--         | c - biteFrame0 >= framesPerBite =
+--           let newEntries = map (,(biteFrame0,biteFrameN)) accClips
+--           in  ((c,c), [], newEntries ++ accAssocList)
+--       getMap (_,_,m) = m
+--   in  Map.fromList . reverse . getMap $ (foldl' aux state0 clipFrames :: F)
+
 clipBites :: Double -> Double -> [Int] -> Map.Map Int (Int,Int)
 clipBites fps biteSize clipFrames =
   let framesPerBite = ceiling $ biteSize * fps
-      state0 = ((0,0), [], [])
-      aux :: F ->  Int -> F
-      aux ((biteFrame0,biteFrameN), accClips, accAssocList) c
-        | c - biteFrame0 < framesPerBite =
-          ((biteFrame0, c), c:accClips, accAssocList)
-        | c - biteFrame0 >= framesPerBite =
-          let newEntries = map (,(biteFrame0,biteFrameN)) accClips
-          in  ((c,c), [], newEntries ++ accAssocList)
-      getMap (_,_,m) = m
-  in  Map.fromList . reverse . getMap $ (foldl' aux state0 clipFrames :: F)
+      binIndex c    = c `div` framesPerBite
+      clips         = zip clipFrames (tail clipFrames)
+      clipGroups    = List.groupBy
+                      (\(s1,e1) (s2,e2) -> binIndex s1 == binIndex s2) clips
+      groupBite g   = (Prelude.minimum (map fst g), Prelude.maximum (map snd g))
+      clipBites     = Prelude.concat $
+                      map (\g -> map (\c -> (fst c, groupBite g)) g)
+                      clipGroups
+  in  Map.fromList $ clipBites
 
 
 modBasename :: (Text -> Text) -> FilePath -> FilePath
@@ -80,81 +86,52 @@ toDir inFile targetDir = directory targetDir </> filename inFile
 
 
 -------------------------------------------------------------------------------
--- | Make a movie clip by looking in $baseDir/tmp for a big fragment
---   containing these frames
-makeClip :: Int
-            -- ^ Length to pad frame index to, (6 for < 1e6 frames)
-         -> Map.Map Int (Int,Int)
-         -- ^ Clip to bite mapping
-         -> Double
-         -- ^ Frames per second of base movie
-         -> (Int,Int)
-         -- ^ First and last frames in this clip
-         -> FilePath
-         -- ^ $baseDir
-         -> IO ()
-makeClip nameLength biteDB fps fs@(frameStart,frameEnd) baseDir = do
-  let toTS    = toTimestamp fps
+-- | Filename for given video clip
+clipFiles :: Double
+          -> Map.Map Int (Int,Int)
+          -> (Int,Int)
+          -> FilePath
+          -> FilePath
+          -> (FilePath, FilePath)
+clipFiles fps bitesDB fs baseName outDir =
+  let (bS,bE)  = fromMaybe (error $ "Bite db lookup error: " ++ show (fst fs))
+                 $ Map.lookup (fst fs) bitesDB
+      inFile   = biteFile fps (bS,bE) baseName outDir
+      outFile  = outDir </> toFileName fps (filename baseName) fs
+  in  (inFile, outFile)
 
-      (bT,bL) = fromMaybe (error "Bite DB lookup error") $
-                Map.lookup frameStart biteDB
+biteFile :: Double -> (Int,Int) -> FilePath -> FilePath -> FilePath
+biteFile fps fs baseName outDir =
+  outDir </> "tmp" </> toFileName fps (filename baseName) fs
 
-      -- Filename of the bite to use ( (100,200) -> )
-      inFile  = toFileName nameLength fps baseDir (bT,bL)
-                (baseDir </> "tmp")
-      (t,l)   = (toTS frameStart, toTS (frameEnd - frameStart))
-      outName = toFileName nameLength fps inFile fs baseDir
-
-      cmd     = T.unwords ["ffmpeg -i"
+extractFrames :: Double -> (Int,Int) -> FilePath -> FilePath -> IO ()
+extractFrames fps (frameStart, frameEnd) inFile outFile = do
+  let cmd     = T.unwords ["ffmpeg -i"
                           ,either (const "") id (toText inFile)
-                          ,"-ss " <> t
+                          ,"-ss " <> (toTimestamp fps frameStart)
                           -- ,"-c copy" -- The copy command will seek to the
                                         -- nearest keyframe. A no-no for
                                         -- short clips like ours
-                          ,"-t " <> l
-                          ,either (const "") id (toText outName)
+                          ,"-t " <> (toTimestamp fps frameEnd)
+                          ,either (const "") id (toText outFile)
                           ]
   echo cmd
+  print $ unwords ["inFile:", show inFile, "outName:", show outFile]
   shell cmd empty
   return ()
-
-
-makeBite :: Int -> FilePath -> Double -> (Int,Int) -> FilePath -> IO ()
-makeBite nameLength inputFile fps fs@(frameStart,frameEnd) outDir = do
-  let toTS    = toTimestamp fps
-      (t,l)   = (toTS frameStart, toTS (frameEnd - frameStart))
-      outName = toFileName nameLength fps inputFile fs
-                (outDir <> "tmp/")
-      cmd     = T.unwords ["ffmpeg -i"
-                          ,either (const "") id (toText inputFile)
-                          ,"-ss " <> t
-                          -- ,"-c copy" -- The copy command will seek to the
-                                        -- nearest keyframe. A no-no for
-                                        -- short clips like ours
-                          ,"-t " <> l
-                          ,either (const "") id (toText outName)
-                          ]
-  echo cmd
-  shell cmd empty
-  return ()
-
 
 ------------------------------------------------------------------------------
-toFileName :: Int
-           -- ^ Name to pad index to in filename. 6 -> 3 -> "000003"
-           -> Double
+toFileName :: Double
            -- ^ Frames per second of base video
            -> FilePath
-           -- ^ Input video file, just used for its name root, e.g. "HomeAlone"
+           -- ^ Input video file, e.g. "HomeAlone.mp4"
            -> (Int,Int)
            -- ^ Frame delimiters for this clip
            -> FilePath
-           -- ^ Base directory for saving all clips
-           -> FilePath
-toFileName nameLength fps inputFile (frameStart, frameEnd) baseDir =
+toFileName fps movieName (frameStart, frameEnd) =
   let numT    = repr frameEnd
       numStr  = T.replicate (nameLength - T.length numT) "0" <> numT
-  in modBasename (<> ("_" <> numStr)) $ inputFile `toDir` baseDir
+  in filename $ modBasename (<> ("_" <> numStr)) movieName
 
 
 ------------------------------------------------------------------------------
@@ -165,20 +142,39 @@ toTimestamp fps nframe =
       t    = tBase { utctDayTime = realToFrac secs } :: UTCTime
   in  T.take 12 . T.pack $ formatTime defaultTimeLocale "%H:%M:%S.%q" t
 
+
 ------------------------------------------------------------------------------
 main :: IO ()
 main = do
   (inFile,cutFile,outDir,fps,byteSize) <- options "Break video into files" optsParser
   let nameLen = 6
-  sh $ do
-    cutFrames <- Turtle.fold (getTimes cutFile) Fold.list
+  do
+    cutFrames <- (0:) <$> Turtle.fold (getTimes cutFile) Fold.list
     let bitesDB = clipBites fps byteSize cutFrames
 
-    mkdir $ outDir </> "tmp"
+    mktree $ outDir </> "tmp"
     forM_ (List.nub $ Map.elems bitesDB) $ \(b0,bN) ->
-      liftIO $ makeBite nameLen inFile fps (b0,bN)
-               (toFileName nameLen fps inFile (b0,bN) (outDir </> "tmp"))
+      let outFile = biteFile fps (b0, bN) inFile outDir
+      in  extractFrames fps (b0,bN) inFile outFile
 
     forM_ (zip (0:cutFrames) cutFrames) $ \fs ->
-      liftIO $ makeClip 6 bitesDB outDir fps fs (toFileName 6 fps outDir fs outDir)
+      let (clipIn, clipOut) = clipFiles fps bitesDB fs inFile outDir
+      in  extractFrames fps fs clipIn clipOut
   echo "Done!"
+
+
+tmpIn :: FilePath -> FilePath
+tmpIn = (</> "tmp")
+
+------------------------------------------------------------------------------
+getTimes :: FilePath -> Shell Int
+getTimes fp = fmap (read . T.unpack) $ input fp
+
+-- TODO testing stuff - delete
+test :: IO ([Int], Map.Map Int (Int,Int))
+test = do
+  cFrames <- Turtle.fold (getTimes "/Users/greghale/Documents/Video/HomeAlone2/cuts.txt") Fold.list
+  return (0:cFrames, clipBites 23.978 20 (0:cFrames))
+
+rawVid = "/Users/greghale/Documents/Video/HomeAlone2/Home_Alone_2_PG.mp4" :: FilePath
+oDir   = "/Users/greghale/Documents/Video/HomeAlone2/testmp4/tmp" :: FilePath
